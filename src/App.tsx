9707cc2a94a2f3590f7063c2125ab3bf1d8bb783
Santiago <santizurlo05@gmail.com>
2026-09-03 15:11:22 -0300

    Eliminacion de cosas antiguas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Admin from './pages/Admin'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <Home />
 }
 
-export default App  
+export default App
